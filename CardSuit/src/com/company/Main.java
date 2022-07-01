f645afd2377c768e216b17fd52b0bb76ec954787
MariaIvanova01 <mimi200801@abv.bg>
2022-07-01 13:17:44 +0300

    Using enumeration

--- CardSuit/src/com/company/Main.java
+++ CardSuit/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        System.out.println("Card Suits:");
+
+        Arrays.stream(CardSuits.values())
+                .forEach(cardSuits ->
+                        System.out.printf("Ordinal value: %d; Name value: %s%n",cardSuits.ordinal(), cardSuits.name()));
+
     }
 }
